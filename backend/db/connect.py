8d824225112e2055184007300e9d6c0e9277a783
Kazim68 <abdurrehmankazim68@gmail.com>
2025-04-25 01:52:39 +0500

    added

--- backend/db/connect.py
+++ backend/db/connect.py
@@ -5,7 +5,5 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-#MONGODB_URI = "mongodb+srv://uchihaItachiTrueHero:UchihaItachiTrueHero@cluster0.swmsudh.mongodb.net/?retryWrites=true&w=majority"
 client = AsyncIOMotorClient(os.getenv("MONGO_URL"))
-#client = AsyncIOMotorClient(MONGODB_URI)
 engine = AIOEngine(client=client, database="forgery_detection")
