085b0246356dd502f516c776e5276aa449b44d17
Kazim68 <120156815+Kazim68@users.noreply.github.com>
2025-05-02 11:09:05 -0700

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -14,7 +14,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  
+    allow_origins=["https://document-forgery-detection.vercel.app/"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
